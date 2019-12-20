a565a5e7e18b48563a5dd8eb3ad88abf6c90dd55
Ewa Sobolewska <ewa.sobolewska@westwing.pl>
2019-12-20 19:57:25 +0100

    reading data

--- crimes project code.R
+++ crimes project code.R
@@ -1,7 +1,20 @@
 
 # Intro -------------------------------------------------------------------
 
-setwd("C:/Users/esobolewska/Desktop/moje własne nie ruszać/studia/ADV R projetk/crimes new")
+setwd("~/crimes new")
+
+library(readr)
+crime <- read_csv("data/crime_and_incarceration_by_state.csv")
+prison <- read_csv("data/prison_custody_by_state.csv")
+ucr <- read_csv("data/ucr_by_state.csv")
+
+head(crime)
+head(prison)
+head(ucr)
+
+str(crime)
+str(prison)
+str(ucr)
 
 # first analysis ----------------------------------------------------------
 
