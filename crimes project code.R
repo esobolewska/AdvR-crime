2a5e01d4d7c2f8f7b9aa691158796c0fb89921e4
Ewa Sobolewska <ewa.sobolewska@westwing.pl>
2019-12-20 19:53:36 +0100

    added first version + data

---
+++ crimes project code.R
@@ -0,0 +1,8 @@
+
+# Intro -------------------------------------------------------------------
+
+setwd("C:/Users/esobolewska/Desktop/moje własne nie ruszać/studia/ADV R projetk/crimes new")
+
+# first analysis ----------------------------------------------------------
+
+
